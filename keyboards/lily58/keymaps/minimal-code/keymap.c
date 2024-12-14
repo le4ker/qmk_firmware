1e971220ff220b0424cbc3bf9ef6e8881da10fb0
Panos Sakkos <panos.sakkos@gmail.com>
2024-12-14 10:11:26 +0200

    don't format layout codes

--- keyboards/lily58/keymaps/minimal-code/keymap.c
+++ keyboards/lily58/keymaps/minimal-code/keymap.c
@@ -13,7 +13,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "keycodes.h"
 #include QMK_KEYBOARD_H
 
 enum layer_number {
@@ -38,7 +37,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                   `----------------------------'           '------''--------------------'
      */
 
-    [_BASE] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LBRC, KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_DEL, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_ENT, KC_MINS, KC_EQL, TG(_LOWER)),
+    // clang-format off
+    [_BASE] = LAYOUT(
+                    KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,              KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
+                    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,               KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
+                    KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G,               KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+                 KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LBRC,     KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_DEL,
+                    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,                  KC_ENT, KC_MINS, KC_EQL, TG(_LOWER)
+    ),
 
     /* LOWER
      * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -55,7 +61,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                   `----------------------------'           '------''--------------------'
      */
 
-    [_LOWER] = LAYOUT(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO, KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO, KC_DEL, KC_LCTL, KC_LALT, KC_LGUI, KC_MPLY, KC_MUTE, KC_BRMD, KC_BRMU, TG(_LOWER))};
+    [_LOWER] = LAYOUT(
+                    KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,           KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
+                    KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5,               KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
+                    KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,          KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
+                KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO, KC_DEL,
+                    KC_LCTL, KC_LALT, KC_LGUI, KC_MPLY,                 KC_MUTE, KC_BRMD, KC_BRMU, TG(_LOWER))};
+
+// clang-format on
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     if (!is_keyboard_master()) {
